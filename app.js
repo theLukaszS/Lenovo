9d548c1d2fd4c06287e227862fb4b16f470609b1
Łukasz S <thelukfx@gmail.com>
2023-08-14 13:55:13 +0200

    slide bar

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener("scroll", function() {
+    const textContent = document.querySelector(".text-content-part-one");
+    const boundingRect = textContent.getBoundingClientRect();
+
+    if (boundingRect.top <= window.innerHeight * 0.8) {
+        textContent.style.opacity = "1";
+        textContent.style.transform = "translate(-50%, -50%)";
+    }
+});
