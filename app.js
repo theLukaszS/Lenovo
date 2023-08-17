29c1331ce6bfea5ffe47d7a6901041c08f2ac4b1
Łukasz S <thelukfx@gmail.com>
2023-08-17 09:41:54 +0200

    slider 2 and 3 done

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-document.addEventListener("scroll", function() {
-    const textContent = document.querySelector(".text-content-part-one");
-    const boundingRect = textContent.getBoundingClientRect();
-
-    if (boundingRect.top <= window.innerHeight * 0.8) {
-        textContent.style.opacity = "1";
-        textContent.style.transform = "translate(-50%, -50%)";
-    }
-});
-
-
 const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
